8cf4edac1f9d262e3a2f05f12e0c46341a3d9503
Thaufiq Umardi <thaufiqumardii@gmail.com>
2018-01-28 19:50:01 +0700

    Add Module Rekam Medik

---
+++ application/modules/Rekammedik/controllers/Rekammedik.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+   *
+   */
+  class Rekammedik extends CI_Controller
+  {
+
+    function __construct(){
+      parent::__construct();
+
+    }
+    function index(){
+      $this->load->view('index');
+    }
+
+  }
+
+ ?>
